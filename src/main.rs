224eec209ba63bff5a9763ad2bd3d04fe3a4b92c
rasteiro11 <titico0306@gmail.com>
2022-03-20 16:06:06 -0300

    end of the chapetr

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::{fs::File, io, time::SystemTime};
+
+use sdl2::{event::Event, keyboard::Keycode};
+
 type Piece = Vec<Vec<u8>>;
 type States = Vec<Piece>;
+
+const LEVEL_TIMES: [u32; 10] = [1000, 850, 700, 600, 500, 400, 300, 250, 221, 190];
+const LEVEL_LINES: [u32; 10] = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200];
+const NB_HIGHSCORES: usize = 5;
+const HIGHSCORE_FILE: &'static str = "scores.txt";
 
 struct Tetrimino {
     states: States,
@@ -103,6 +112,7 @@ impl Tetris {
     }
 
     fn make_permanent(&mut self) {
+        let mut to_add = 0;
         if let Some(ref mut piece) = self.current_piece {
             let mut shift_y = 0;
             while shift_y < piece.states[piece.current_state as usize].len()
@@ -123,13 +133,16 @@ impl Tetris {
                 }
                 shift_y += 1;
             }
+            to_add += self.current_level;
         }
         self.check_lines();
+        self.update_score(to_add);
         self.current_piece = None;
     }
 
     fn check_lines(&mut self) {
         let mut y = 0;
+        let mut score_add = 0;
         while y < self.game_map.len() {
             let mut complete = true;
             for x in &self.game_map[y] {
@@ -139,15 +152,107 @@ impl Tetris {
                 }
             }
             if complete == true {
+                score_add += self.current_level;
                 self.game_map.remove(y);
                 y -= 1;
             }
             y += 1;
         }
+
+        if self.game_map.len() == 0 {
+            score_add += 1000;
+        }
+        self.update_score(score_add);
+
         while self.game_map.len() < 16 {
+            self.increase_line();
             self.game_map.insert(0, vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         }
     }
+
+    fn update_score(&mut self, to_add: u32) {
+        self.score += to_add;
+    }
+
+    fn increase_line(&mut self) {
+        self.nb_lines += 1;
+        if self.nb_lines > LEVEL_LINES[self.current_level as usize - 1] {
+            self.current_level += 1;
+        }
+    }
+}
+
+fn handle_events(
+    tetris: &mut Tetris,
+    quit: &mut bool,
+    timer: &mut SystemTime,
+    event_pump: &mut sdl2::EventPump,
+) -> bool {
+    let mut make_permanent = false;
+    if let Some(ref mut piece) = tetris.current_piece {
+        let mut tmp_x = piece.x;
+        let mut tmp_y = piece.y;
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    *quit = true;
+                    break;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => {
+                    *timer = SystemTime::now();
+                    tmp_y += 1;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    tmp_x += 1;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    tmp_x -= 1;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => {
+                    piece.rotate(&tetris.game_map);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
+                    ..
+                } => {
+                    let x = piece.x;
+                    let mut y = piece.y;
+                    while piece.change_position(&tetris.game_map, x, y + 1) == true {
+                        y += 1;
+                    }
+                    make_permanent = true;
+                }
+                _ => {}
+            }
+        }
+
+        if !make_permanent {
+            if piece.change_position(&tetris.game_map, tmp_x, tmp_y) == false && tmp_y != piece.y {
+                make_permanent = true;
+            }
+        }
+    }
+    if make_permanent {
+        tetris.make_permanent();
+        *timer = SystemTime::now();
+    }
+    make_permanent
 }
 
 trait TetriminoGenerator {
@@ -364,4 +469,93 @@ impl TetriminoGenerator for TetriminoT {
     }
 }
 
+fn is_time_over(tetris: &Tetris, timer: &SystemTime) -> bool {
+    match timer.elapsed() {
+        Ok(elapsed) => {
+            let millis = elapsed.as_secs() as u32 * 1000 + elapsed.subsec_nanos() / 1_000_000;
+            millis > LEVEL_TIMES[tetris.current_level as usize - 1]
+        }
+        Err(_) => false,
+    }
+}
+
+fn update_vec(v: &mut Vec<u32>, value: u32) -> bool {
+    if v.len() < NB_HIGHSCORES {
+        v.push(value);
+        v.sort();
+        true
+    } else {
+        for entry in v.iter_mut() {
+            if value > *entry {
+                *entry = value;
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn write_into_file(content: &str, file_name: &str) -> io::Result<()> {
+    let mut f = File::create(file_name)?;
+    f.write_all(content.as_bytes())
+}
+
+fn read_from_file(file_name: &str) -> io::Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn slice_to_string(slice: &[u32]) -> String {
+    slice
+        .iter()
+        .map(|highscore| highscore.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn save_highscores_and_lines(highscores: &[u32], number_of_lines: &[u32]) -> bool {
+    let s_highscores = slice_to_string(highscores);
+    let s_number_of_lines = slice_to_string(number_of_lines);
+    write_into_file(
+        &format!("{}\n{}\n", s_highscores, s_number_of_lines),
+        HIGHSCORE_FILE,
+    )
+    .is_ok()
+}
+
+fn print_game_information(tetris: &Tetris) {
+    let mut new_highest_highscore = true;
+    let mut new_highest_lines_sent = true;
+    if let Some((mut highscores, mut lines_sent)) = load_highscores_and_lines() {
+        new_highest_highscore = update_vec(&mut highscores, tetris.score);
+        new_highest_lines_sent = update_vec(&mut lines_sent, tetris.nb_lines);
+        if new_highest_highscore || new_highest_lines_sent {
+            save_highscores_and_lines(&highscores, &lines_sent);
+        }
+    } else {
+        save_highscores_and_lines(&[tetris.score], &[tetris.nb_lines]);
+    }
+    println!("Game over...");
+    println!(
+        "Score:           {}{}",
+        tetris.score,
+        if new_highest_highscore {
+            " [NEW HIGHSCORE]"
+        } else {
+            ""
+        }
+    );
+    println!(
+        "Number of lines: {}{}",
+        tetris.nb_lines,
+        if new_highest_lines_sent {
+            " [NEW HIGHSCORE]"
+        } else {
+            ""
+        }
+    );
+    println!("Current level:   {}", tetris.current_level);
+}
 fn main() {}
